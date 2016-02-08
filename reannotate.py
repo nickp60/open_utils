b670491c42c414bca00c6edde8da1f2be3a02c30
nickp60 <nickp60@gmail.com>
2016-02-08 17:31:13 -0500

    works, needs more flexibility

--- reannotate.py
+++ reannotate.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 24 13:39:09 2015
-######################## Database Re-Annotation Tool ##########################
+######################## Genome Re-Annotation Tool ##########################
 #####################         By Nick Waters 2015        ######################
 ##################               Brinsmade Lab                #################
 @author: Nick Waters
 
-#                             Version 4.0-
+#                             Version 4.1
 #                               20151217
 #
 #   About:  this script is essentially a wrapper for NCBI BLAST+, the 
@@ -17,9 +17,12 @@ Created on Tue Feb 24 13:39:09 2015
 #   -argparse
 #   -better debug tools
 #   -fixed date using strtime
-#   -fixed the hack to recase malformed dataframe (index, record iterations, etc)
+#   -fixed the hack to recast malformed dataframe (index, record iterations, etc)
 #
-#   Minor version revisions: 
+#   Minor version revisions:
+#   -made it work
+#
+# 
 #   Requires: -installation of BioPython and NCBI Blast+ standalone suite
 #             -make a directory called "BLAST" in your home folder
 #             
@@ -30,7 +33,7 @@ Created on Tue Feb 24 13:39:09 2015
 #   OUTPUT: this will return a csv with the genome reannotations for used with 
 #           downstream R applications.  Additionally, it will output the BLAST
 #           XML for use with anything else.
-#           Additionally, it will spit out a protein (or DNA) fasta wuth your 
+#           Additionally, it will spit out a protein (or DNA) fasta with your 
 #           genome
 #   Known Bugs:  Might crash and burn if attempted on a PC.
                 But who knows? I tried to use the os.path stuff for handling 
@@ -50,13 +53,14 @@ import numpy as np
 from Bio.Alphabet import IUPAC
 from Bio.Blast import NCBIXML
 from Bio.Blast.Applications import NcbiblastpCommandline
-DEBUG=True
+DEBUG=False
 #%% 
 #define inputs
 remake_blast_db=False
+nuc_flag=True
 if DEBUG:
-    input_genome = os.path.expanduser("~/GitHub/Py/uams1.gb")
-    input_target_fasta = os.path.expanduser("~/Downloads/subtilis168.fasta")
+    input_genome = os.path.expanduser("~/GitHub/BlastDBs/subtilis168.gb")
+    input_target_fasta = os.path.expanduser("~/GitHub/BlastDBs/uniprot300.fasta")
     nuc_flag=True
     remake_blast_db=True
 else:
@@ -114,34 +118,22 @@ for record in SeqIO.parse(input_handle, "genbank"):
     for index, feature in enumerate(record.features):
         if feature.type != "CDS":
             continue
-        # "4" is to avoid the first entry (source) and some early possible tRNA genes)
-        if record.features[4].qualifiers.has_key("old_locus_tag"):
-            try:
+        try:
+            if feature.qualifiers.has_key("old_locus_tag"):
                 genbankdf.loc[index, "old_locus_tag"]=str(feature.qualifiers["old_locus_tag"]).replace("\'", "").replace("[", "",).replace("]", "")
-                genbankdf.loc[index, "locus_tag"]=str(feature.qualifiers["locus_tag"]).replace("\'", "").replace("[", "",).replace("]", "")
-                genbankdf.loc[index, "product"]      =str(feature.qualifiers["product"]).replace("\'", "").replace("[", "",).replace("]", "")
-                genbankdf.loc[index, "protein_id"]   =str(feature.qualifiers["protein_id"]).replace("\'", "").replace("[", "",).replace("]", "") 
-                genbankdf.loc[index, "db_xref"]      =str(feature.qualifiers["db_xref"]).replace("\'", "").replace("[", "",).replace("]", "")
-                genbankdf.loc[index, "seq"]          =str(feature.location.extract(record).seq)
-                genbankdf.loc[index, "translation"]  =str(feature.qualifiers["translation"]).replace("\'", "").replace("[", "",).replace("]", "")        
-                genbankdf.loc[index, "type"]="CDS"
-            except KeyError:
-                genbankdf.loc[index, "type"] = "pseudo" 
-        else: 
-            try: 
+            else:
                 genbankdf.loc[index, "old_locus_tag"]="none"
-                genbankdf.loc[index, "locus_tag"]=str(feature.qualifiers["locus_tag"]).replace("\'", "").replace("[", "",).replace("]", "")
-                genbankdf.loc[index, "product"]=str(feature.qualifiers["product"]).replace("\'", "").replace("[", "",).replace("]", "")
-                genbankdf.loc[index, "protein_id"]=str(feature.qualifiers["protein_id"]).replace("\'", "").replace("[", "",).replace("]", "") 
-                genbankdf.loc[index, "db_xref"]=str(feature.qualifiers["db_xref"]).replace("\'", "").replace("[", "",).replace("]", "")
-                genbankdf.loc[index, "seq"]=str(feature.location.extract(record).seq)
-                genbankdf.loc[index, "translation"]=str(feature.qualifiers["translation"]).replace("\'", "").replace("[", "",).replace("]", "")        
-                genbankdf.loc[index, "type"]="CDS"
-            except KeyError:
-                genbankdf.loc[index, "type"] = "pseudo" 
+            genbankdf.loc[index, "locus_tag"]=str(feature.qualifiers["locus_tag"]).replace("\'", "").replace("[", "",).replace("]", "")
+            genbankdf.loc[index, "product"]      =str(feature.qualifiers["product"]).replace("\'", "").replace("[", "",).replace("]", "")
+            genbankdf.loc[index, "protein_id"]   =str(feature.qualifiers["protein_id"]).replace("\'", "").replace("[", "",).replace("]", "") 
+            genbankdf.loc[index, "db_xref"]      =str(feature.qualifiers["db_xref"]).replace("\'", "").replace("[", "",).replace("]", "")
+            genbankdf.loc[index, "seq"]          =str(feature.location.extract(record).seq)
+            genbankdf.loc[index, "translation"]  =str(feature.qualifiers["translation"]).replace("\'", "").replace("[", "",).replace("]", "")        
+            genbankdf.loc[index, "type"]="CDS"
+        except KeyError:
+            genbankdf.loc[index, "type"] = "pseudo" 
 genbankdf.reset_index(level=0, inplace=True)
-#%%      shake it out from wide to tall data, sanity check, and 
-##       set up recipient structures
+#%% set up recipient structures
 #print(genbankdf.loc[genbankdf['locus_tag'] == 'QV15_00005'])   ### just a test      
 
 
@@ -161,7 +153,7 @@ def prepare_for_blastn(x):
                     '|'+str(content[4])+
                     '|'+str(content[5])))
         nseqList.append(b)
-#%%
+
 if nuc_flag=="T":
     noutput_handle=open(os.path.join(subdirname, gb+"n.fasta"),"w") #  Nucleotide output
     noutput_path = os.path.join(subdirname, gb+"n.fasta")   #  Nucleotide output
@@ -172,7 +164,7 @@ if nuc_flag=="T":
 else: pass
 #%%  make that dataframe into a protein fasta  
 print(str("Writing out protein .fasta to \n"+ aaoutput_path+"..."))
-def preparefor_blastaa(x):
+def prepare_for_blastaa(x):
     for content in x.itertuples():
         for locus_tag in content[3]:
             if locus_tag in aaseqList:
@@ -185,8 +177,8 @@ def preparefor_blastaa(x):
                     '|'+str(content[4])+
                     '|'+str(content[5])))
         aaseqList.append(b)
-preparefor_blastaa(genbankdf)
-SeqIO.write(aaseqList, aaoutput_handle, "fasta")    #####USE WITH CAUTION- BIG FILE
+prepare_for_blastaa(genbankdf)
+SeqIO.write(aaseqList, aaoutput_handle, "fasta")
 
  #%%  close open handles
 input_handle.close()
@@ -228,110 +220,112 @@ else:
 #%% Blast query against target
  #path to blast database for target 
 bdb_path=os.path.join(os.path.abspath("BLAST"), bdb_name)
+bdb_path_recip=os.path.join(os.path.abspath("BLAST"), gb)
 # build commandline call
 blast_cline = NcbiblastpCommandline(query=aaoutput_path, 
                                     db= bdb_path,  evalue=0.001,
                                     outfmt=5, out=blast_path)
-# last I checked, I couldnt figure out how to add this through the python blast api                                    
-add_params=" -num_threads 2 -max_target_seqs 1"
-#print(str(blast_cline)+add_params)
-blast_command=str(str(blast_cline)+add_params)
-print("Running BLAST search...")
-subprocess.Popen(blast_command, stdout=subprocess.PIPE,  shell=True).stdout.read()
-print(str("Writing out XML to "+blast_path+"..."))
-
-#%% Blast target against query
- #path to blast database for query
-bdb_path_recip=os.path.join(os.path.abspath("BLAST"), gb)
-# build commandline call
 blast_cline_recip = NcbiblastpCommandline(query=input_target_fasta, 
                                     db= str(bdb_path_recip+"_db"),  evalue=0.001,
                                     outfmt=5, out=blast_path_recip)
-add_params=" -num_threads 2 -max_target_seqs 1"
+# last I checked, I couldnt figure out how to add this through the python blast api                                    
+add_params=" -num_threads 2 -max_target_seqs 1 -max_hsps_per_subject 1"
 #print(str(blast_cline)+add_params)
+blast_command=str(str(blast_cline)+add_params)
 blast_command_recip=str(str(blast_cline_recip)+add_params)
 print("Running BLAST search...")
+subprocess.Popen(blast_command, stdout=subprocess.PIPE,  shell=True).stdout.read()
+print(str("Writing out XML to "+blast_path+"..."))
+print("Running reciprocal BLAST search...")
 subprocess.Popen(blast_command_recip, stdout=subprocess.PIPE,  shell=True).stdout.read()
 print(str("Writing out XML to "+blast_path_recip+"..."))
-#%%  mae dfs with ids for query and target
-#TODO is this needed anymore?
-#input_handle.open
-targetdf=querydf =pd.DataFrame(columns=["locus_tag", "match_in_target","bit_score","gaps"])
-#targetdf=pd.DataFrame(columns=["locus_tag", "match_in_query","bit_score","gaps"])
-for index, i in enumerate(genbankdf.locus_tag):
-    querydf.loc[index, "locus_tag"]=i
-
-for index, i in enumerate(SeqIO.parse(open(input_target_fasta), "fasta")):
-    targetdf.loc[index, "id"]=i.id
 #%%Parse ( from http://stackoverflow.com/questions/1684470/biopython-extracting-sequence-ids-from-a-blast-output-file)          
-# fill in data frames
 print("Parsing XML")
 # for iteration in query against fasta, return hit and stat
 record = NCBIXML.parse(open(blast_path))
 record_recip = NCBIXML.parse(open(blast_path_recip))
 def xml_to_df(record):
-    df=pd.DataFrame(columns=["locus_tag", "match_in_target","bit_score","gaps"])
+    df=pd.DataFrame(columns=["locus_tag", "match_in_target","bit_score","gaps","ortho"])
     counter=1    
     for rec in record:
         locus=str(rec.query.partition(" ")[2].partition("|")[0])
         if locus in df.locus_tag.values:
-            print("Caution, possible duplicate %s;  locus_tag must be unique" %locus)
+            print("Caution, possible duplicate %s;  locus_tag must be unique" %i)
             line_index=counter
             df.loc[line_index, "locus_tag"]=locus
             counter=counter+1            
             continue
         else:
            for alignment in rec.alignments:
-               for hsp in alignment.hsps:
-                   line_index=counter
-                   df.loc[line_index, "locus_tag"]=locus
-                   df.loc[line_index, "match_in_target"]=alignment.hit_def
-                   df.loc[line_index, "bit_score"]      =hsp.bits
-                   df.loc[line_index, "gaps"]           =hsp.gaps
-                   counter=counter+1            
-
-                   
+               hsp = alignment.hsps[0] #zero allows selection of first HSP only, as max_hsps_per_subject seems to fail 
+               line_index=counter
+               df.loc[line_index, "locus_tag"]=locus
+               df.loc[line_index, "match_in_target"]=alignment.hit_def
+               df.loc[line_index, "bit_score"]      =hsp.bits
+               df.loc[line_index, "gaps"]           =hsp.gaps
+               counter=counter+1            
     return(df)
-querydf=xml_to_df(record)
-targetdf=xml_to_df(record_recip)
+querydf =  xml_to_df(record)
+targetdf = xml_to_df(record_recip)
 #%%
-d2 = pd.DataFrame(targetdf.match_in_target.str.split("|").tolist(), 
-                  columns=genbankdf.columns[2:6],index=np.arange(len(d2))+1 )
-targetdf.match_in_target=d2.locus_tag
+
+d2 = pd.DataFrame(targetdf.match_in_target.str.split("|").tolist(), columns=genbankdf.columns[2:6],index=np.arange(len(targetdf))+1 )
+
+#%%targetdf.match_in_target=d2.locus_tag
 targetdf=targetdf.sort(columns="match_in_target")
 targetdf.reset_index(level=0, inplace=True)
+querydf=querydf.sort(columns="locus_tag")
 
 #%%
 #TODO: use bit score and gaps to create homologue filtering schema
-#%% merge on locus tag
-#    for j in targetdf.loc[1:len(targetdf)].index:
 
-for i in querydf.locus_tag[1:10]:
-    in_querydf_index=querydf[querydf['locus_tag']==i].index.tolist()
-    in_querydf_match=str(querydf["match_in_target"][in_querydf_index].item())
-    in_targetdf_index=targetdf[targetdf["locus_tag"]==in_querydf_match].index.tolist()
-    in_targetdf_match=str(targetdf["locus_tag"][in_targetdf_index].item())
-    if in_querydf_match==in_targetdf_match:
-        print(i+"has found its match!" )
+
+#TODO pretest for uniqueness?
+len(targetdf.match_in_target)!=len(set(targetdf.match_in_target))
+#%% judge reciprocity
+#    for j in targetdf.loc[1:len(targetdf)].index:
+duplicates=list()
+non_reciprocal_loci=list()
+for i in querydf.locus_tag: #for each locus tag in query,
+    index_in_querydf=querydf[querydf['locus_tag']==i].index.tolist() #get the index in the query df
+    if len(index_in_querydf) is not 1:# address issues where something is clearly broken (0), or multiple cases found, also indicating something is broken.  If either, break
+        if len(index_in_querydf) == 0:
+           print(i+" not found in query?")
+           break
+        if len(index_in_querydf) >= 2:
+           print("multiple instances of "+i+"found, using first; handling of multiple hsps is unavailible currently")
+           continue
+    querydf_vs_targetdf_match=str(querydf["match_in_target"][index_in_querydf].item()) #find match query vs target (forward search)
+    index_in_targetdf=targetdf[targetdf["locus_tag"]==querydf_vs_targetdf_match].index.tolist()  #find index for match in target vs query df
+    if len(index_in_targetdf)==0: # if locus tag is absent from target df (ie, non reciprocal result, where best matches arent eachother)
+        print("non-reciprocal matching for %s" %querydf["locus_tag"][index_in_querydf].item())
+        querydf["ortho"][index_in_querydf]="not_reciprocal"
+        non_reciprocal_loci.append(i)
+        continue
+    if len(index_in_targetdf)>1: # if locus tag is present more than once in target df other
+        print("multiple hits for %s" %querydf["locus_tag"][index_in_querydf].item())
+    targetdf_vs_querydf_match=str(targetdf["locus_tag"][index_in_targetdf[0]]) #try first
+    if querydf_vs_targetdf_match==targetdf_vs_querydf_match:
+        querydf["ortho"][index_in_querydf]="reciprocal"
+        continue
+    elif len(index_in_targetdf)>1:
+        print("checking multiple reciprocal hits for %s" %i)
+        for i in range(1,len(index_in_targetdf)):
+            targetdf_vs_querydf_match=str(targetdf["locus_tag"][index_in_targetdf[i]] )#try first
+            if querydf_vs_targetdf_match==targetdf_vs_querydf_match:
+                querydf["ortho"][index_in_querydf]="reciprocal"
+            else:
+                print("womp")
     else:
         print("womp")
-        
-print("in_querydf_index: %s \n in_querydf_match: %s \n in_targetdf_index: %s \n in_targetdf_match: %s" %(in_querydf_index,in_querydf_match,in_targetdf_index,in_targetdf_match))
-#        if targetdf[targetdf["locus_tag"]==in_querydf_match][]:
-#            in_querydf=
-    
+
 
 #%%
-=pd.DataFrame(newdb.items(), columns=['locus_tag', str(bdb_name+'_anno')]).sort(columns='locus_tag', axis=0, ascending=True)
-locus_tag=genbankdf
-new=pd.merge(newdf, locus_tag, on='locus_tag')#, suffixes=['_left', '_right'])
+#TODO make sure this handles all cases well
+new=pd.merge(genbankdf, querydf, on='locus_tag')#, suffixes=['_left', '_right'])
 
-#%% make dataframe, match, amd merge over the "qv_anno" column
-newdf=pd.DataFrame(newdb.items(), columns=['locus_tag', str(bdb_name+'_anno')]).sort(columns='locus_tag', axis=0, ascending=True)
-locus_tag=genbankdf
-new=pd.merge(newdf, locus_tag, on='locus_tag')#, suffixes=['_left', '_right'])
 
 #%% Write new database to output path
 print("Writing results as CSV")
 new.to_csv(finaldbcsv_path)
-
+#TODO write out log file with params, etc
