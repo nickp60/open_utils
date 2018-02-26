c858bbb9c7827d3e19e08f73028fe610194cf021
nickp60 <nickp60@gmail.com>
2018-02-26 18:35:55 +0000

    annofilt kinda works, I guess

--- annofilt/filter_annotations.py
+++ annofilt/filter_annotations.py
@@ -1,4 +1,4 @@
-d#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 @author: Nick Waters
@@ -49,6 +49,10 @@ def get_args():
         action="store_true",
         help="reciprocal blast for stringent checking")
     optional.add_argument(
+        "-p"
+        "--min_percent_id", dest="min_percent_id",
+        help="minimum percentage id", type=float, default=.9)
+    optional.add_argument(
         "-l"
         "--min_length", dest="min_length",
         help="minimum percentage length", type=float, default=.9)
@@ -58,6 +62,12 @@ def get_args():
         dest="min_evalue",
         default=.00005,
         help="minimum e value")
+    optional.add_argument(
+        "-t"
+        "--threads",
+        dest="threads",
+        default=4,
+        help="number of threads to use")
     optional.add_argument("-v", "--verbosity",
                         dest='verbosity', action="store",
                         default=2, type=int,
@@ -100,13 +110,13 @@ def setup_blast_db(input_file, input_type="fasta", dbtype="prot",
         sys.exit(1)
 
 
-def make_prot_nuc_recip_blast_cmds(
-        query_list, date, evalue,
-        output, subject_file=None, logger=None):
+def make_prot_prot_blast_cmds(
+        query_list, date, evalue, output, threads=1,
+        reciprocal=False,subject_file=None, logger=None):
     """given a file, make a blast cmd, and return path to output csv
-    Only works is query_list is nucleotide and subject_file is protein
     """
     assert logger is not None, "must use logging"
+    assert isinstance(threads, int), "threads must be integer"
     logger.info("Creating protein BLAST database")
     db_dir = os.path.join(output,
                           os.path.splitext(os.path.basename(subject_file))[0])
@@ -130,7 +140,7 @@ def make_prot_nuc_recip_blast_cmds(
         blast_cline = NcbiblastpCommandline(query=f,
                                             db=protdb, evalue=evalue,
                                             outfmt=6, out=output_path_tab)
-        add_params = str(" -num_threads 1 -num_alignments 20")
+        add_params = " -num_threads {} -num_alignments 20".format(threads)
         blast_command = str(str(blast_cline) + add_params)
         blast_cmds.append(blast_command)
         blast_outputs.append(output_path_tab)
@@ -143,55 +153,14 @@ def make_prot_nuc_recip_blast_cmds(
                                                    evalue=evalue,
                                                    outfmt=6, out=recip_output_path_tab)
         recip_blast_command = str(str(recip_blast_cline) + add_params)
-        blast_cmds.append(recip_blast_command)
-        recip_blast_outputs.append(recip_output_path_tab)
-
+        if reciprocal:
+            blast_cmds.append(recip_blast_command)
+            recip_blast_outputs.append(recip_output_path_tab)
     return(blast_cmds, blast_outputs, recip_blast_outputs)
 
 
-def get_complete_paths_of_files(directory):
-    filenames = []
-    shortnames = [i for i in os.listdir(directory) if not os.path.isdir(os.path.join(directory, i))]
-    for i in shortnames:
-        filenames.append(os.path.join(directory, i))
-    return(filenames)
 
-
-def merge_outfiles(filelist, outfile_name):
-    """
-    """
-    # only grab .tab files, ie, the blast output
-    filelist = [i for i in filelist if i.split(".")[-1:] == ['tab']]
-    if len(filelist) == 1:
-        # print("only one file found! no merging needed")
-        return(filelist[0])
-    else:
-        # print("merging all the blast results to %s" % outfile_name)
-        nfiles = len(filelist)
-        fout = open(outfile_name, "a")
-        # first file:
-        for line in open(filelist[0]):
-            fout.write(line)
-        #  now the rest:
-        for num in range(1, nfiles):
-            f = open(filelist[num])
-            for line in f:
-                fout.write(line)
-            f.close()  # not really needed
-        fout.close()
-    return(outfile_name)
-
-
-def BLAST_tab_to_df(path):
-    colnames = ["query_id", "subject_id", "identity_perc", "alignment_length",
-                "mismatches", "gap_opens", "q_start", "q_end", "s_start",
-                "s_end", "evalue", "bit_score"]
-    raw_csv_results = pd.read_csv(
-        open(path), comment="#", sep="\t", names=colnames)
-    return raw_csv_results
-
-
-def filter_recip_BLAST_df(df1, df2, min_percent, logger=None):
+def filter_BLAST_df(df1, df2, min_length_percent, min_percent, reciprocal, logger=None):
     """ results from pd.read_csv with default BLAST output 6 columns
     df1 must be genomes against genes, and df2 must be genes against genomes,
     because we have to split the names so all all the contigs are recognized
@@ -199,11 +168,44 @@ def filter_recip_BLAST_df(df1, df2, min_percent, logger=None):
     """
     assert logger is not None, "must use a logger"
     logger.debug("shape of blast results")
-    logger.debug("shape of recip blast results")
     df1['genome'] = df1.query_id.str.split('_').str.get(0)
-    df2['genome'] = df2.subject_id.str.split('_').str.get(0)
     logger.debug(df1.shape)
-    logger.debug(df2.shape)
+
+    if args.reciprocal:
+        logger.debug("shape of recip blast results")
+        logger.debug(df2.shape)
+        df2['genome'] = df2.subject_id.str.split('_').str.get(0)
+
+
+    if not reciprocal:
+        filtered = pd.DataFrame(columns=df1.columns)
+        unq_subject = df1.subject_id.unique()
+        unq_query = df1.genome.unique()
+        recip_hits = []
+        nonrecip_hits = []  # should be renamed to nogood_hits
+
+        for gene in unq_subject:
+            for genome in unq_query:
+                logger.debug("Checking %s in %s for reciprocity" % (gene, genome))
+                tempdf1 = df1.loc[(df1["subject_id"] == gene) &
+                              (df1["genome"] == genome), ]
+                # here we get the best hit (the one maximizing e value)
+                subset1 = tempdf1.loc[
+                    (tempdf1["identity_perc"] > min_percent) &
+                    (tempdf1["bit_score"] == tempdf1["bit_score"].max())]
+                logger.debug("grouped df shape: ")
+                logger.debug(tempdf1.shape)
+                if subset1.empty:
+                    logger.info("No full hits for %s in %s", gene, genome)
+                    logger.debug(tempdf1)
+                    nonrecip_hits.append([gene, genome])
+                else:
+                    if all(subset1["alignment_length"] > subset1["subject_length"] * min_length_percent):
+                        filtered = filtered.append(subset1)
+            # logger.debug(subset.shape)
+        return(filtered)
+
+
     # recip structure
     filtered = pd.DataFrame(columns=df1.columns)
     unq_subject = df1.subject_id.unique()
@@ -407,107 +409,8 @@ def BLAST_tab_to_df(path):
     return raw_csv_results
 
 
-def filter_recip_BLAST_df(df1, df2, min_percent, logger=None):
-    """ results from pd.read_csv with default BLAST output 6 columns
-    df1 must be genomes against genes, and df2 must be genes against genomes,
-    because we have to split the names so all all the contigs are recognized
-    as coming from one genome.  returns a df
-    """
-    assert logger is not None, "must use a logger"
-    logger.debug("shape of blast results")
-    logger.debug("shape of recip blast results")
-    df1['genome'] = df1.query_id.str.split('_').str.get(0)
-    df2['genome'] = df2.subject_id.str.split('_').str.get(0)
-    logger.debug(df1.shape)
-    logger.debug(df2.shape)
-    # recip structure
-    filtered = pd.DataFrame(columns=df1.columns)
-    unq_subject = df1.subject_id.unique()
-    unq_query = df1.genome.unique()
-    recip_hits = []
-    nonrecip_hits = []
-    for gene in unq_subject:
-        for genome in unq_query:
-            logger.debug("Checking %s in %s for reciprocity" % (gene, genome))
-            tempdf1 = df1.loc[(df1["subject_id"] == gene) &
-                              (df1["genome"] == genome), ]
-            tempdf2 = df2.loc[(df2["query_id"] == gene) &
-                              (df2["genome"] == genome), ]
-            if tempdf1.empty or tempdf2.empty:
-                logger.info("skipping %s in %s", gene, genome)
-            else:
-                subset1 = tempdf1.loc[
-                    (tempdf1["identity_perc"] > min_percent) &
-                    (tempdf1["bit_score"] == tempdf1["bit_score"].max())]
-                # (tempdf1["alignement_l"] == tempdf1["bit_score"].max())]
-                subset2 = tempdf2.loc[
-                    (tempdf2["identity_perc"] > min_percent) &
-                    (tempdf2["bit_score"] == tempdf2["bit_score"].max())]
-                logger.debug("grouped df shape: ")
-                logger.debug(tempdf1.shape)
-                logger.debug("grouped df2 shape: " )
-                logger.debug(tempdf2.shape)
-                if subset1.empty or subset2.empty:
-                    logger.info("No reciprocol hits for %s in %s", gene, genome)
-                    logger.debug(tempdf1)
-                    logger.debug(tempdf2)
-                    nonrecip_hits.append([gene, genome])
-                else:
-                    # logger.debug(tempdf1)
-                    # logger.debug("tempdf2")
-                    # logger.debug(tempdf2)
-                    # logger.debug("subset1")
-                    # logger.debug(subset1)
-                    # logger.debug("subset2")
-                    # logger.debug(subset2)
-                    if subset1.iloc[0]["query_id"] == subset2.iloc[0]["subject_id"]:
-                        recip_hits.append([gene, genome])
-                        filtered = filtered.append(subset1)
-                        logger.info("Reciprocol hits for %s in %s!", gene, genome)
-                    else:
-                        nonrecip_hits.append([gene, genome])
-                        logger.info("No reciprocol hits for %s in %s", gene, genome)
-
-            # logger.debug(subset.shape)
-    logger.debug("Non-reciprocal genes:")
-    logger.debug(nonrecip_hits)
-    logger.debug("Reciprocal genes:")
-    logger.debug(recip_hits)
-    logger.debug("filtered shape:")
-    logger.debug(filtered.shape)
-    return(filtered)
-
-    # idx = csv_results.groupby(
-    #     ['subject_id'])['identity_perc'].transform(max) == csv_results['identity_perc']
-    # best_hits_df = csv_results[idx]
-    # logger.debug("Shape of best hits:")
-    # logger.debug(best_hits_df.shape)
-    # new_df = best_hits_df.query('identity_perc >= 95 and evalue < .0001')
-    # # new_df = best_hits_df.query('identity_perc >= 95')
-    # return new_df
 
 
-def write_pipe_extract_cmds(df, outfile, logger=None):
-    #% parse output
-    assert logger is not None, "must use a logger"
-    logger.debug("cleaning up the csv output")
-    with open(outfile, "a") as outf:
-        for index, row in df.iterrows():
-            if row['q_start'] > row['q_end']:
-                logger.debug("hit is on the (-) strand")
-                line = "{0}-RC@{1} :{2}:{3}".format(
-                    row['subject_id'],
-                    row['query_id'],
-                    int(row['q_end']),
-                    int(row['q_start']))
-            else:
-                line = "{0}@{1} :{2}:{3}".format(
-                    row['subject_id'],
-                    row['query_id'],
-                    int(row['q_start']),
-                    int(row['q_end']))
-            sys.stdout.write(line + "\n")
-            outf.write(line + "\n")
 
 if __name__ == "__main__":
 
@@ -550,12 +453,13 @@ if __name__ == "__main__":
         raise ValueError("no .faa file found in prokka dir!")
     if len(query_gb) > 1:
         raise ValueError("Multiple genbank files found in prokka ouput!")
-    if reciprocal
     commands, paths_to_outputs, paths_to_recip_outputs = \
-        make_prot_nuc_recip_blast_cmds(
+        make_prot_prot_blast_cmds(
             query_list=query_gb,
             evalue=args.min_evalue,
+            reciprocal=args.reciprocal,
             subject_file=args.ref_gb,
+            threads=args.threads,
             output=output_root, date=date,
             logger=logger)
     logger.debug("cmds:" )
@@ -565,6 +469,7 @@ if __name__ == "__main__":
     logger.debug("paths to recip outputs:" )
     logger.debug(paths_to_recip_outputs )
     # check for existing blast results
+
     if not all([os.path.isfile(x) for x in paths_to_outputs]):
         pool = multiprocessing.Pool()
         logger.debug("Running the following commands in parallel " +
@@ -590,16 +495,35 @@ if __name__ == "__main__":
                                     "recip_merged_results.tab")
     post_merged_tab = merge_outfiles(filelist=paths_to_outputs,
                                             outfile_name=merged_tab)
-    post_recip_merged_tab = merge_outfiles(filelist=paths_to_recip_outputs,
-                                           outfile_name=recip_merged_tab)
     resultsdf = BLAST_tab_to_df(post_merged_tab)
-    recip_resultsdf = BLAST_tab_to_df(post_recip_merged_tab)
-    filtered_hits = filter_recip_BLAST_df(
+    if args.reciprocal:
+        recip_resultsdf = BLAST_tab_to_df(post_recip_merged_tab)
+        post_recip_merged_tab = merge_outfiles(filelist=paths_to_recip_outputs,
+                                               outfile_name=recip_merged_tab)
+    else:
+        recip_resultsdf = None
+    # go through the subject file to aretrieve the ideal lengths.
+    with open(args.ref_gb, "r") as inf:
+        resultsdf["subject_length"] = 0
+        for rec in SeqIO.parse(inf, "fasta"):
+            resultsdf.ix[resultsdf.subject_id==rec.id, 'subject_length'] = len(rec.seq)
+    # sanity check that this worked
+    if all(resultsdf["subject_length"] == 0):
+        raise ValueError(
+            "error matching blast gene names to those parsed by " +
+            "BioPython!  Until this is sorted, we wont be able to " +
+            "match up the total expected gene length. Exiting")
+
+    filtered_hits = filter_BLAST_df(
         df1=resultsdf,
         df2=recip_resultsdf,
-        min_percent=args.min_length,
+        reciprocal=args.reciprocal,
+        min_percent=args.min_percent_id,
+        min_length_percent=args.min_length,
         logger=logger)
     print(filtered_hits)
+    filtered_hits.to_csv(
+        os.path.join(output_root, "simpleOrtho_filtered_hits.csv"))
     write_pipe_extract_cmds(
         outfile=os.path.join(output_root,
                              "simpleOrtho_regions.txt"),
