efb61422d3689f6ec9fe6e197ac47294a23c64eb
Nick Waters <nickp60@gmail.com>
2020-02-20 10:43:46 +0000

    add tests to fix ob1

--- vcfortless/vcfortless/main.py
+++ vcfortless/vcfortless/main.py
@@ -11,7 +11,7 @@ from collections import namedtuple
 from operator import attrgetter
 # import io
 from Bio import SeqIO
-# from Bio.Seq import Seq
+from Bio.Seq import Seq
 # from Bio.SeqRecord import SeqRecord
 # from Bio.SeqIO.FastaIO import SimpleFastaParse
 
@@ -20,7 +20,7 @@ from Bio import SeqIO
 # SYN: 0 synonymous, 1 non-synonymous
 # EFF: Effect, one of Premature Stop Codon, putative PROmoter disruption inTERgenic, inTRAgenic
 Result = namedtuple('Result', 'CHROM  POS REF ALT BI CHG SYN EFF')
-
+DEBUG=False
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -30,6 +30,10 @@ def get_args():
     parser.add_argument("-t", "--trans_table",
                         help="translation table; default 11",
                         type=int, default=11)
+    parser.add_argument("-f", "--feature",
+                        help="which feature to consider: gene or cds",
+                        choices=["gene", "cds"],
+                        default="gene")
     parser.add_argument("-b", "--binding_width",
                         help="width to mark whether snps might disrupt RBS",
                         type=int, default=15)
@@ -63,16 +67,51 @@ def tvts(ref, alt):
     result = refs[ref.upper()][alt.upper()]
     return (result)
 
+
+def subs_nuc(refseq, start, end, pos, alt):
+    #gene ------
+    # ----;----;
+    #        *
+    # snp: 8
+    # start = 5
+    # start = 4 in python, but biopython uses 0-based, but vcf used 1-based
+    # end = 10
+    # region seq[5 - 1: 8 - 1 ] + SNP + seq[ SNP : end ]
+    thisseq =  refseq[start : pos ] + alt +refseq[pos + 1  : end ]
+    # print(refseq[start: end])
+    # print(thisseq)
+    assert len(thisseq) == len(refseq[start: end]), "bad reconstruction of  reference"
+    return thisseq
+
+def test_subs_nuc_psc():
+    refseq = "ATGCCCAAATTTTACTAG"
+    mutseq = "ATGCCCAAATTTTAGTAG"
+    newseq =  subs_nuc(refseq, start=0, end=18, pos=14, alt="G")
+    assert   mutseq == newseq, "error in sub_nuc function"
+
+def test_subs_nuc_norm():
+    refseq = "ATGCCCAAATTTTACTAG"
+    mutseq = "ATGCCCAAATTTTATTAG"
+    assert mutseq == subs_nuc(refseq, start=0, end=18, pos=14, alt="T"), "error in sub_nuc function"
+
+def test_pmc():
+    refseq = "ATGCCCAAATTTTACTAG"
+    mutseq = "ATGCCCAAATTTTAGTAG"
+    thisp = Seq(mutseq).translate(table=11, to_stop=True)
+    refp = Seq(refseq).translate(table=11, to_stop=True)
+    print(thisp)
+    print(refp)
+    assert len(refp) != len(thisp), "error detecting premature stop codon!"
+
 def process_region(args, vcf_data, chrom, start, end, rec, strand, is_locus=False):
     if is_locus:
         assert rec is not None, "must provide rec for loci"
         assert strand is not None, "must provide feature for loci"
-        nucseq  = rec.seq[start-1: end]
+        nucseq  = rec.seq[start: end]
         if strand == 1:
             nucseqp = nucseq.translate(table=args.trans_table, to_stop=True)
         else:
             nucseqp = nucseq.reverse_complement().translate(table=args.trans_table, to_stop=True)
-
     these_vcfs = vcf_data[chrom][start: end]
     ignored = 0
     for pos, ref, altlist, PROCESS in these_vcfs:
@@ -88,27 +127,28 @@ def process_region(args, vcf_data, chrom, start, end, rec, strand, is_locus=Fals
         for alt in altlist:
             thiststv = tvts(ref, str(alt))
             if is_locus:
-                #gene ------
-                # ----;----;
-                #        *
-                # snp: 8
-                # start = 5
-                # start = 4 in python, buy biopython uses trad numbering
-                # end = 10
-                # region seq[5 - 1: 8 - 1 ] + SNP + seq[ SNP : end ]
-                thisseq =  rec.seq[start - 1: pos - 1] + ref +rec.seq[pos : end]
+                thisseq = subs_nuc(rec.seq, start, end, pos, str(alt))
+
                 assert len(thisseq) == len(nucseq), "bad reconstruction of  reference"
                 if strand == 1:
                     thisseqp = thisseq.translate(table=args.trans_table, to_stop=True)
                 else:
                     thisseqp = thisseq.reverse_complement().translate(table=args.trans_table, to_stop=True)
+                if DEBUG:
+                    print(nucseq)
+                    print(thisseq)
+
+                    print(rec.seq[start : pos ] + ref)
+                    print(nucseqp)
+                    print(thisseqp)
                 SYN = 1
                 EFF = "TER"
                 if nucseqp != thisseqp:
                     SYN = 0
                     if len(thisseqp) != len(nucseqp):
                         EFF = "PSC"
-                thisres = Result(chrom, pos, ref, alt, bialleleic, thiststv, SYN, EFF)
+                # back to 1-indexed
+                thisres = Result(chrom, pos+1, ref, alt, bialleleic, thiststv, SYN, EFF)
                 sys.stdout.write("%s\t%i\t%s\t%s\t%i\t%s\t%i\t%s\n" % thisres)
             else:
                 # process intergenic region
@@ -156,17 +196,16 @@ def main(args=None):
     for i, v in enumerate(vcf_reader):
         # if (i % 1000) == 0:
         #     sys.stderr.write(str(i) + " ")
-        if v.POS > 100000000:
+        if v.POS > 200000000:
+            sys.stderr.write("Warning: long sequence detected, only processing the first 20M")
             break
-        # if v.CHROM not in vcf_data.keys():
-        #     vcf_data[v.CHROM] = []
         while counter != v.POS and counter < v.POS:
             vcf_data[v.CHROM].append([counter, "-", "-", False])
             counter = counter + 1
         assert counter == v.POS, "error syncing counters"
-        vcf_data[v.CHROM].append([v.POS, v.REF, v.ALT, True])
+        # make 0-indexed
+        vcf_data[v.CHROM].append([v.POS-1, v.REF, v.ALT, True])
         counter = counter + 1
-
     last_gene_end = 0
     # first process all the coding sequences, then hit the remaining intergenic loci
     ignored_positons = 0
@@ -175,7 +214,8 @@ def main(args=None):
             thischrom = rec.id.split(".")[0]
             sys.stderr.write("Processing %s\n" % thischrom)
             for feat in rec.features:
-                if feat.type not in ["source"]:
+                #if feat.type not in ["source"]:
+                if feat.type == args.feature:
                     # process coding region
                     ig = process_region(
                         args, vcf_data,
@@ -186,7 +226,6 @@ def main(args=None):
                         strand=feat.strand,
                         is_locus=True)
                     ignored_positons = ignored_positons + ig
-                    # process previous intergenic region
                     ig = process_region(
                         args, vcf_data,
                         chrom=thischrom,
